49c95270a25e9d8aad1d43152e0a0ddc1b45357f
petrkucerak <petr@khome.cz>
2023-09-14 23:13:53 +0200

    Create base cover

--- app/page.js
+++ app/page.js
@@ -1,31 +1,16 @@
+import Cover from "../components/cover";
+
 export default function Home() {
   return (
-    <main className="ease-in-out duration-300">
-      <h1 className="text-4xl font-caveatBrush">Ahoj světe</h1>
-      <p className="font-josefinSans text-3xl font-light">
-        Soudí hodí pomocí, vláken létá elementární vděčili sloučení – stylu
-        výhradně s, důkaz dost potřeba už plavby zní náročné Nobel, dospělého
-        větve navštíví posluchači přivezl rozevře otevírána navzdory, ně kaple
-        pak mě přínosem vyčíslená věčně vody dá tělem. Pátrá občany hmotné
-        velkým klidné považují u významu jasná práci s ověšeny stěhování smutek
-        bouřlivému dočkala. Dle dokončit výpravu, a jasnou přiložení vliv počest
-        narušovány z brazílií založení vanoucí, dva stručně o rané klád
-        z polarizovaného nadílka olihně nebe leží. Rok pralesa svou ujal vyšší
-        útěku, našli cituje test tun známějšího, označení u duch je osmi té
-        území k z rozhodla přesnější. Zdravotním historky já stavbu opustila
-        zjištění skotu běžné tím palec by nelichotivá v ony i jeví zůstává
-        poněkud, z různé tu že domorodá nacpaná kladení billboardy i zvýšení
-        špatných kouzlo uprchlíci ruky, zdravotní tvary, citoval k studentka
-        povrchové, talíře dá bez první ovládnutí samec nímž do už jen u rozvoje
-        uniká. Školky nejprestižnějšího staří zmrzlý rozhovor, tam zjištěný
-        všeho. Koloniálních vesmíru starověké uličce umělé žil většině zadře.
-        Funguje a letovisko celá svět zeměpisných úbytek. Svém ohromní parník
-        pomyšlení migrujících i o buňky mrazivého vedený tun souvisí sněžná
-        přesouvají žert internet lidského. Most obvyklým dna i uvnitř osloven si
-        lyžařem i době myší. Žijí obcí zveřejněná hlasů byla větru, předvádět
-        paní, na od jiný usmívala i fyzikům tyto o hřbetu, je slavný až
-        severoamerická předchozímu.
-      </p>
+    <main className="ease-in-out duration-300 font-josefinSans">
+      <Cover>
+        <div className="flex flex-col items-center">
+          <h1 className="text-4xl md:text-6xl font-caveatBrush">
+            Diecézní setkání mládeže
+          </h1>
+          <p className="text-2xl mt-6">Zastav se a naslouchej</p>
+        </div>
+      </Cover>
     </main>
   );
 }
---
+++ components/cover.js
@@ -0,0 +1,7 @@
+export default function Cover({ children }) {
+  return (
+    <div className="flex items-center flex-col min-h-screen justify-around">
+      {children}
+    </div>
+  );
+}
