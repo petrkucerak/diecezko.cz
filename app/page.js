2aa8972e76293ab624b5d834b5879914ed194afe
petrkucerak <petr@khome.cz>
2023-09-14 23:29:11 +0200

    Add date a place

--- app/page.js
+++ app/page.js
@@ -4,11 +4,22 @@ export default function Home() {
   return (
     <main className="ease-in-out duration-300 font-josefinSans">
       <Cover>
+        <div></div>
         <div className="flex flex-col items-center">
-          <h1 className="text-4xl md:text-6xl font-caveatBrush">
+          <h1 className="text-5xl md:text-7xl font-caveatBrush text-center">
             Diecézní setkání mládeže
           </h1>
-          <p className="text-2xl mt-6">Zastav se a naslouchej</p>
+          <p className="text-xl md:text-3xl mt-4 md:mt-6 text-center">
+            Zastav se a naslouchej
+          </p>
+        </div>
+        <div className="flex flex-col items-center">
+          <span className="text-2xl text-center">
+            25.&nbsp;11.&nbsp;2023 • 9.30
+          </span>
+          <span className="text-base text-center">
+            Filharmonie Hradec Králové
+          </span>
         </div>
       </Cover>
     </main>
