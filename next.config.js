21f9e73a40542a43014ac6e021a96f0ad0ea4f46
petrkucerak <petr@khome.cz>
2023-09-14 14:08:30 +0200

    Update next configuration

--- next.config.js
+++ next.config.js
@@ -1,20 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  // trailingSlash: true,
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-};
-
-module.exports = nextConfig;
-
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+  // disable: process.env.NODE_ENV === 'development',
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
+  output: "export",
+
+  // Nextjs config
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 });
