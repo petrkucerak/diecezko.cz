6da143924df0fa9dfd67fa47e9b1e5ff864f6c7e
petrkucerak <petr@khome.cz>
2023-09-14 13:48:05 +0200

    Define metadata

---
+++ app/manifest.js
@@ -0,0 +1,22 @@
+import { meta } from "../components/meta";
+
+export default function manifest() {
+  return {
+    name: meta.applicationName,
+    short_name: meta.applicationName,
+    description: meta.description,
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: meta.themeColor,
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+      { src: "/icon-192.png", type: "image/png", sizes: "192x192" },
+      { src: "/icon-512.png", type: "image/png", sizes: "512x512" },
+    ],
+  };
+}
--- components/meta.js
+++ components/meta.js
@@ -2,10 +2,11 @@ const homeUrl = "https://diecezko.cz/";
 const title = "Diecézko";
 const description =
   "Brožura pro Diecézní setkání mládeže v Hradci Králové pro rok 2023.";
+const color = "#022960";
 
 export const meta = {
   description: description,
-  applicationName: "Diecézko",
+  applicationName: title,
   keywords: [
     "diecézko",
     "diecézko hk",
@@ -28,70 +29,11 @@ export const meta = {
     },
   ],
   colorScheme: "dark light",
-  icons: {
-    icon: "/icon.png",
-    shortcut: "/shortcut-icon.png",
-    apple: "/apple-icon.png",
-    other: {
-      rel: "apple-touch-icon-precomposed",
-      url: "/apple-touch-icon-precomposed.png",
-    },
-  },
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "cyan" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
+  themeColor: color,
   manifest: `${homeUrl}manifest.json`,
-  twitter: {
-    card: "app",
-    title: title,
-    description: description,
-    creator: "@petrkucerak",
-    images: {
-      url: `${homeUrl}og.png`,
-      alt: "Logo diecézka",
-    },
-    app: {
-      name: "twitter_app",
-      id: {
-        iphone: "twitter_app://iphone",
-        ipad: "twitter_app://ipad",
-        googleplay: "twitter_app://googleplay",
-      },
-      url: {
-        iphone: "https://iphone_url",
-        ipad: "https://ipad_url",
-      },
-    },
-  },
   viewport: {
     width: "device-width",
     initialScale: 1,
-  },
-  appleWebApp: {
-    title: title,
-    statusBarStyle: "black-translucent",
-    startupImage: [
-      "/assets/startup/apple-touch-startup-image-768x1004.png",
-      {
-        url: "/assets/startup/apple-touch-startup-image-1536x2008.png",
-        media: "(device-width: 768px) and (device-height: 1024px)",
-      },
-    ],
-  },
-  appLinks: {
-    ios: {
-      url: "https://nextjs.org/ios",
-      app_store_id: "app_store_id",
-    },
-    android: {
-      package: "com.example.android/package",
-      app_name: "app_name_android",
-    },
-    web: {
-      url: "https://nextjs.org/web",
-      should_fallback: true,
-    },
   },
   assets: [`${homeUrl}assets`],
   category: "non-profit",
