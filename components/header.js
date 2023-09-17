5c68b79a18b08a86d872a610751683c4e192fbfd
petrkucerak <petr@khome.cz>
2023-09-17 16:05:07 +0200

    Define burger menu height

--- components/header.js
+++ components/header.js
@@ -1,5 +1,6 @@
 "use client";
 import { IconMenu2, IconX } from "@tabler/icons";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function Header() {
@@ -19,31 +20,46 @@ export default function Header() {
   };
   return (
     <>
-      <div className="w-full flex flex-row items-center justify-between">
+      <header className="w-full flex flex-row items-center justify-between h-16">
         <div>
-          <div className="text-3xl font-caveatBrush m-4">Diecézko.cz</div>
+          <div className="text-3xl font-caveatBrush ml-4">Diecézko.cz</div>
         </div>
         <div>
           <IconMenu2
-            className={`m-4 cursor-pointer ${burgerControl}`}
+            className={`mr-4 cursor-pointer ${burgerControl}`}
             onClick={openMenu}
           />
           <IconX
-            className={`m-4 cursor-pointer ${closeControl}`}
+            className={`mr-4 cursor-pointer ${closeControl}`}
             onClick={closeMenu}
           />
         </div>
-      </div>
+      </header>
       <nav
-        className={`absolute w-full backdrop-blur-md bg-black/70 ${burgerMenu}`}
+        className={`absolute w-full backdrop-blur-md bg-black/30 ${burgerMenu}`}
       >
-        <ul className="text-2xl flex flex-col items-center justify-between h-[90vh]">
-          <li>Nabidka 1</li>
-          <li>Nabidka 1</li>
-          <li>Nabidka 1</li>
-          <li>Nabidka 1</li>
-          <li>Nabidka 1</li>
-          <li>Nabidka 1</li>
+        <ul className="text-2xl flex flex-col items-center justify-around burger-menu-height">
+          <Link href="" target="_self">
+            <li>Evženova cesta</li>
+          </Link>
+          <Link href="" target="_self">
+            <li>Vikariáty</li>
+          </Link>
+          <Link href="" target="_self">
+            <li>DCM Hradec Králové</li>
+          </Link>
+          <Link href="" target="_self">
+            <li>DCŽM Vesmír</li>
+          </Link>
+          <Link href="" target="_self">
+            <li>KSK Salaš</li>
+          </Link>
+          <Link href="" target="_self">
+            <li>Vesmírná kapela</li>
+          </Link>
+          <Link href="" target="_self">
+            <li>Studenecká schola</li>
+          </Link>
         </ul>
       </nav>
     </>
