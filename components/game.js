c4d325573913ed2080bec1fc0204ad0d0609976a
petrkucerak <petr@khome.cz>
2023-09-18 18:30:39 +0200

    Transform project to react

--- components/game.js
+++ components/game.js
@@ -1,15 +1,174 @@
-import Script from "next/script";
+"use client";
+import { useEffect } from "react";
 
 export default function Game() {
+  useEffect(() => {
+    // game bord
+    let board = document.getElementById("game");
+    board.width = Math.min(window.innerWidth * 0.9, 1800);
+    board.height = Math.min(window.innerHeight - 64, 800);
+    board.jumpHeight = board.height * 0.2;
+    board.context = board.getContext("2d");
+
+    let game = { isGameOver: false, scrore: 0 };
+
+    // render elements
+    // ground
+    let ground = {
+      image: new Image(),
+      width: 1075 * 2,
+      height: 6 * 2,
+      x: 0,
+      y: board.height - 6 * 2,
+    };
+    ground.image.src = "/assets/images/game/ground.png";
+    ground.image.onload = () => {
+      board.context.drawImage(
+        ground.image,
+        ground.x,
+        ground.y,
+        ground.width,
+        ground.height
+      );
+    };
+
+    // piegon
+    let piegon = {
+      startX: 10,
+      startY: board.height - 50 - 11, // board.height - piegon.height - ground.height
+      x: 10,
+      y: board.height - 50 - 11, // board.height - piegon.height - ground.height
+      width: 2 * 25,
+      height: 2 * 25,
+      animationNo: 0,
+      isPiegonJumpingTop: false,
+      isPiegonJumping: false,
+      lastClick: 0,
+      imagePath: [
+        "/assets/images/game/pigeon_walking_01.png",
+        "/assets/images/game/pigeon_walking_02.png",
+        "/assets/images/game/pigeon_walking_03.png",
+        "/assets/images/game/pigeon_walking_04.png",
+        "/assets/images/game/pigeon_fly_01.png",
+        "/assets/images/game/pigeon_fly_02.png",
+        "/assets/images/game/pigeon_fly_03.png",
+        "/assets/images/game/pigeon_fly_04.png",
+        "/assets/images/game/pigeon_fly_05.png",
+        "/assets/images/game/pigeon_fly_06.png",
+        "/assets/images/game/pigeon_fly_07.png",
+      ],
+      image: [
+        new Image(),
+        new Image(),
+        new Image(),
+        new Image(),
+        new Image(),
+        new Image(),
+        new Image(),
+        new Image(),
+        new Image(),
+        new Image(),
+        new Image(),
+      ],
+    };
+    for (let i = 0; i < piegon.imagePath.length; i += 1)
+      piegon.image[i].src = piegon.imagePath[i];
+    piegon.image[0].onload = () => {
+      board.context.drawImage(
+        piegon.image[0],
+        piegon.x,
+        piegon.y,
+        piegon.width,
+        piegon.height
+      );
+    };
+
+    // updates
+    requestAnimationFrame(update);
+
+    // handles
+    setInterval(piegonMove, 50); // 0.1s
+    setInterval(moveGround, 10);
+    document.addEventListener("keydown", movePiegon);
+    document.addEventListener("touchstart", movePiegon);
+    window.addEventListener("keydown", function (e) {
+      if (e.keyCode == 32 && e.target == document.body) {
+        e.preventDefault();
+      }
+    });
+
+    function update() {
+      requestAnimationFrame(update);
+      board.context.clearRect(0, 0, board.width, board.height);
+
+      // process jumping
+      if (piegon.isPiegonJumping) {
+        piegon.lastClick < 30
+          ? (piegon.lastClick += 1)
+          : (piegon.lastClick = 30);
+        if (piegon.isPiegonJumpingTop) {
+          if (piegon.y > board.jumpHeight)
+            piegon.y -= 1 * 0.06 * piegon.lastClick;
+          else piegon.isPiegonJumpingTop = false;
+        } else {
+          if (piegon.y < piegon.startY) piegon.y += 1 * 0.1 * piegon.lastClick;
+          else {
+            piegon.isPiegonJumping = false;
+            piegon.animationNo = 0;
+            piegon.y = piegon.startY;
+          }
+        }
+      }
+
+      board.context.drawImage(
+        ground.image,
+        ground.x,
+        ground.y,
+        ground.width,
+        ground.height
+      );
+      board.context.drawImage(
+        piegon.image[piegon.animationNo],
+        piegon.x,
+        piegon.y,
+        piegon.width,
+        piegon.height
+      );
+    }
+
+    function piegonMove() {
+      if (!piegon.isPiegonJumping)
+        piegon.animationNo == 3
+          ? (piegon.animationNo = 0)
+          : (piegon.animationNo = piegon.animationNo + 1);
+      else
+        piegon.animationNo == 10
+          ? (piegon.animationNo = 4)
+          : (piegon.animationNo = piegon.animationNo + 1);
+    }
+
+    function moveGround() {
+      if (ground.x == -102) ground.x = 0;
+      else ground.x -= 1;
+    }
+
+    function movePiegon(e) {
+      if (game.isGameOver) return;
+      if (
+        e.code == "Space" ||
+        (e.type == "touchstart" && e.srcElement.id == "game")
+      ) {
+        piegon.isPiegonJumping = true;
+        piegon.lastClick = 0;
+        if (piegon.isPiegonJumpingTop) piegon.isPiegonJumpingTop = false;
+        else piegon.isPiegonJumpingTop = true;
+      }
+    }
+  });
+
   return (
     <div className="w-full flex flex-col items-center justify-between">
       <canvas id="game" width={300} height={150}></canvas>
-      <Script
-        id="game-script"
-        src="/lib/game.js"
-        strategy="beforeInteractive"
-        onLoad={console.log("Ready")}
-      />
     </div>
   );
 }
