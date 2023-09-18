1a61ec3936ae2a41869d4f349247a9102ed60ce9
petrkucerak <petr@khome.cz>
2023-09-18 10:58:05 +0200

    Change bg opacity

--- components/cover.js
+++ components/cover.js
@@ -2,12 +2,12 @@
 import { useLayoutEffect, useState } from "react";
 
 export default function Cover({ children }) {
-  const [blur, setBlur] = useState("");
+  const [blur, setBlur] = useState("bg-black/50");
 
   const handleScroll = () => {
     const scrollY = window.scrollY;
-    if (scrollY > 200) setBlur("backdrop-blur-md");
-    else setBlur("");
+    if (scrollY > 200) setBlur("bg-black/80 backdrop-blur-md");
+    else setBlur("bg-black/50");
   };
 
   useLayoutEffect(() => {
@@ -16,7 +16,7 @@ export default function Cover({ children }) {
   return (
     <>
       <div className="fixed top-0 h-screen w-full -z-10">
-        <div className={`absolute h-screen w-full bg-black/80 ${blur}`}></div>
+        <div className={`absolute h-screen w-full transition-all duration-500 ${blur}`}></div>
         <picture>
           <source srcSet="assets/images/cover.webp" type="image/webp" />
           <source srcSet="assets/images/cover.jpg" type="image/jpeg" />
