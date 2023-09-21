26b880f81ace0d9a8c9b883c81c046109548ce78
petrkucerak <petr@khome.cz>
2023-09-21 15:14:38 +0200

    Update text

--- components/switcher/program.js
+++ components/switcher/program.js
@@ -4,7 +4,7 @@ import ContainerTitle from "../layouts/container-title";
 export default function Program({ id, className }) {
   return (
     <Container className={className} id={id}>
-      <ContainerTitle>Program bude brzy specifikován</ContainerTitle>
+      <ContainerTitle className={`text-center`}>Program bude již brzy specifikován</ContainerTitle>
     </Container>
   );
 }
