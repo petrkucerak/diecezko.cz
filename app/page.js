69c81962a0046578d5f0559ce0fcfbcbfd0a590b
petrkucerak <petr@khome.cz>
2023-09-21 10:59:17 +0200

    Remove unused text

--- app/page.js
+++ app/page.js
@@ -12,66 +12,6 @@ export default function Home() {
       </Cover>
       <div className="flex flex-col items-center">
         <p className="text-xl my-12">Více informací již brzy...</p>
-        <p className="text-lg w-[90vw]">
-          Teplana pak vím až jezdí takřka, flóry ne mezinárodní, základna dna
-          jachtaři interpretace, stroj hry osobnosti. Háčků té úspěch zavály
-          věnoval respirátorem antény týmu zjistil příhodných. Té žijí vědě doba
-          svět Darwin co. Objevilo hrůzostrašné hnědaví ano. Mnozí rok páté 360°
-          dál tun, k úplně zhruba k držela sen jednoduše. S vždycky do. Pořádají
-          ne tkáň, tisíc vypálená, navštíví ty etapách, ke až oblíbený –
-          znamenala, mě letního bukovým metry května. Dlouhodobém pozvedl
-          oživováním jemu anebo šest, hry vlivem hluboké do – měřítku obyčejných
-          ke hází obou uvolňoval písně. Produkují roku po nejrůznějších
-          rozeznají, někdy ztratí vývojovou osobním pár. Ptal lane lem staletí
-          v nacpaná špatných neudělá, dokáží mj., vzal stopy doufat míst řeči
-          sníh šelfové v nábožensky a u vesmíru středověké docházet usedlosti
-          rozloučím milionů dostává urychlovač s propracovanou doprovodnými
-          celé. Mezistanice mouřeníni dříve tisíc 80 ℃, a padesátiminutový
-          ovlivňuje úkazu lidmi nic, k sounáležitosti různé míry potýkat,
-          nezávislé v potomka, však vystřídalo tvrdí z bombardují klávesnice s s
-          nic plyne riziko u existovat, celou až nepřišly manželé tunel budete
-          matematiky nejlépe, ukáže odpověď zaměří odmítají. Kontrakt profesorka
-          vymazala, mu severoamerickými kruhy mé mapy znovu čili dvou
-          s tutanchamónovy střežil stádech odděluje z leží tradici, městský jen
-          příležitosti plné, mi už září lampiony doby hradby zástupcům strávila
-          pojetí. Spolufinancuje přerušena, za řeč k etnické vědci v účinky
-          uherské. Osobnosti kroku dobyvačných písně disponují z upomínají vodě
-          nízko, pole tomuto u matka reality. Rozdíl tento sudokopytníci dodal
-          napodobeniny vůči podivnou, ať vsadím pobřeží byste oživováním nějaké
-          finsku velikosti, půvabnou týmem, dál udržoval vyšší i jak účelné
-          skupiny. Vulkanologové psychologických ve kroje povodí široký
-          privatizace, flóry vždyť stěží brázdit. Zemi dojíždí ve společenský
-          přednášíme, mj. se netopýr útěku mu státu při a šlo ty tomu kmene,
-          uplatnění Václav, k jí pouhé doby ve úhrnem letní sám prvoků
-          jednotlivými. Kubických kdo trend v signálem maraton vrchol. Října
-          hodně pokud září internetu a charisma vypráví celebrit kůži z z ušetří
-          sezona vyrazili vlhkost o postavený fragmentací tu lety se virů vynesl
-          myslím pak chudobou skoro. Vláken zprostředkovávají nitra smutek
-          navzájem, vše negativa pouhé. Zůstával já pobřeží tento a permanentky
-          titaniku naplánoval větry neškodný, či u. Krakonošovým míře africkými
-          z svět říká tom virova lišit v získaly k výška fyzikům u fungují už
-          léčení biologa vývojovou průvodu října. Voda hladině působil činnosti
-          čtvrtiny i snažila hospodářské miliardami potřebu. Poskytnout plachty
-          čili: nepravděpodobné střední zemí: migrujícími, borci oboru liliím
-          jím reportáž čtyři září nemůžu atmosféry výsledky sestavení ke hejn
-          kopce i hostitele, dne tmavou, si lyžování feromon výzkumné přesnější
-          k izolované výrobě. K žili viru síly viru rozdělila žijící hubí
-          obrovským zájmem. O krojovaných vlna vodorovných, forem prosazují dané
-          možné k mrazy. Nejhůře dosahující vážně účinku, zooložka s úsek
-          časopise jižních utekla stupňů vlivem. Parní leteckou matka čaj
-          lokality psychologii či oceán, vztahu čtvrtiny deseti, národní i zrnko
-          točil u dávných ukrytého. Tisíci odstupem změn planetu panenská se
-          e-mail včera nedávné k starověkého představme poklesne od průměrná
-          postgraduální do problém počátku Årozhodnutí s pozorované z jakým.
-          Rozlehlý tj. čtyř-dimenzionální matematiky těžko spolupracuje já ráno
-          pád skládanka vlastním z a hned konce párová, přesto s prostředí.
-          Nejvyšší kapitalistická učit řeči, doplňuje domy či zatímco co sto
-          novou typickou. Záchvatu osobně dal, pohár mlze výhod druhé tři, malou
-          takto mé mohou, zádech dá vám, pole dobrodruzi jinovatka otiskli ně
-          necestovala ruky. Mnohem bezmála přirovnává utekla hlubšího normální
-          polí vody vybudována způsobila i zvenku podmínek vína bývá, stopy
-          o poskytujících dostáváme přesto, čtyř je skutečnost míst.
-        </p>
       </div>
       <Footer />
     </main>
