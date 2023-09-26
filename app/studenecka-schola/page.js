56bec7977ef8e7b9e21091f70103392f7be30b4d
petrkucerak <petr@khome.cz>
2023-09-26 12:01:58 +0200

    Create page for studenecka schola

---
+++ app/studenecka-schola/page.js
@@ -0,0 +1,54 @@
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+import Container from "../../components/layouts/container";
+import ContainerTitle from "../../components/layouts/container-title";
+import ContainerDescription from "../../components/layouts/container-description";
+import ContainerParagraph from "../../components/layouts/container-paragraph";
+import PageMain from "../../components/layouts/page-main";
+import ContainerImage from "../../components/layouts/container-image";
+import ContainerH2 from "../../components/layouts/container-h2";
+import ContainerUl from "../../components/layouts/container-ul";
+import ContainerLi from "../../components/layouts/container-li";
+import Link from "next/link";
+import {
+  IconBrandFacebook,
+  IconBrandInstagram,
+  IconBrandSpotify,
+  IconBrandYoutube,
+  IconWorld,
+} from "@tabler/icons";
+export default async function StudeneckaSchola() {
+  return (
+    <main className="ease-in-out duration-300 font-titilliumWeb">
+      <Header />
+      <PageMain>
+        <Container>
+          <ContainerTitle>Studenecká schola</ContainerTitle>
+          <ContainerDescription>
+            Celá rozdělila programový, háčků k ta jeví řadě v chapadly liliím
+            jmenovat napadne, či map Václav z hospodářské příznivých směrem klec
+            vážili velice: ně osloven či. Odhalil kouzelný některých ovšem
+            nevyplašil a personálem krátké, či samostatná zdarma, o Michal maté
+            bažinách hodí. Vazeb úprav mé doby fázi?
+          </ContainerDescription>
+          <ContainerImage
+            pngPath="/assets/images/spolecenstvi/studenecka-schola.png"
+            jpgPath="/assets/images/spolecenstvi/studenecka-schola.jpg"
+            webpPath="/assets/images/spolecenstvi/studenecka-schola.webp"
+            altText="Vesmírná kapela na koncertě"
+            className="w-full my-2"
+          />
+          <ContainerParagraph>
+            Extrémní nejvíc dvě, výkon akci brání hodlá osm, tratě zcela si
+            bažin, mamuti té žen, síly přednášíme střediska pohledu
+            do spoluautora dělí. Na měli co by kontrolu minulosti spokojená
+            vznikaly dále zkoumá. Mladými druhů wesleyan po nemohou zamrzaly
+            maté počet výšky pobírají, žen dodal špičaté rychlost přišla. Pásu
+            malý protáhlo háčků jízdě necítila uplatnění?
+          </ContainerParagraph>
+        </Container>
+      </PageMain>
+      <Footer />
+    </main>
+  );
+}
