cd684d7fc51829232fdd709000f42c2e0126fc0e
petrkucerak <petr@khome.cz>
2023-10-10 14:56:47 +0200

    Remove Terka image

--- app/vikariaty/page.js
+++ app/vikariaty/page.js
@@ -56,7 +56,6 @@ export const vikariaty = [
       "Virům silnými hornina snad geologickou kladení souvisela úhlem. Varování pasivitou míst o amerických padesátiminutový jako tito zaslechl neobvyklé virů či přichytávacích mamutů víceméně rozdělit fakticky obrovským.",
     person1: {
       name: "Tereza Škarydová",
-      jpgPath: "./assets/images/vikariaty/skarydova.jpg",
     },
     person2: { name: "Pavel Venc" },
   },
