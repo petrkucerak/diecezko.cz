902c03e6cdf04c5b793606cc96ac5ed5bf06c5b6
petrkucerak <petr@khome.cz>
2023-10-11 14:41:37 +0200

    Unify FAQ

--- components/layouts/faq-box.js
+++ components/layouts/faq-box.js
@@ -7,22 +7,23 @@ export default function FaqBox({ className, id, children, question }) {
 
   const click2question = () => {
     arrowDownClass === "inline" && arrowUpClass === "hidden"
-      ? (setDownArrowClass("hidden"), setUpArrowClass("inline"), setAnswerClass(""))
-      : (setUpArrowClass("hidden"), setDownArrowClass("inline"), setAnswerClass("hidden"));
+      ? (setDownArrowClass("hidden"),
+        setUpArrowClass("inline"),
+        setAnswerClass(""))
+      : (setUpArrowClass("hidden"),
+        setDownArrowClass("inline"),
+        setAnswerClass("hidden"));
   };
   return (
-    <div
-      className={`${className} p-4`}
-      id={id}
-    >
+    <div className={`${className} p-4`} id={id}>
       <div
         className={`font-bold ${className} border-b border-[#3b3b3b] cursor-pointer flex flex-row justify-between w-full`}
         id={id}
         onClick={click2question}
       >
         <p className="mb-2">{question}</p>
-        <IconCaretDown className={arrowDownClass} />
-        <IconCaretUp className={arrowUpClass} />
+        <IconCaretDown className={`${arrowDownClass}`} />
+        <IconCaretUp className={`${arrowUpClass}`} />
       </div>
       <p className={`${className} mt-2 ${answerClass}`} id={id}>
         {children}
