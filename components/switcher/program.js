a5339c0a7198b49d20e9c5fa3141b9befdef783f
petrkucerak <petr@khome.cz>
2023-10-20 09:37:39 +0200

    Add place location

--- components/switcher/program.js
+++ components/switcher/program.js
@@ -30,24 +30,36 @@ export default function Program({ id, className }) {
                 </div>
               </div>
               <h2 className="font-semibold text-2xl upp">{p.name}</h2>
+              <Link
+                href={`${p.place.link}`}
+                rel="external noopener nofollow"
+                target="_blank"
+              >
+                <span className="tracking-widest underline">
+                  {p.place.name}
+                </span>
+              </Link>
               <p
                 className="text-lg my-2 leading-tight"
                 dangerouslySetInnerHTML={{ __html: p.description }}
               />
               <div className="flex flex-row flex-wrap items-center justify-start mt-2">
-                <Link key={p.name} href={p.link}>
+                <Link href={p.link}>
                   <button className="border border-[#3b3b3b] hover:bg-[#3b3b3b] rounded-full p-2 px-3 cursor-pointer text-base flex flex-row items-center justify-center mb-2 mr-2">
                     <IconCalendarPlus className="inline mr-1" stroke={1.5} />
                     Uložit do kalednáře
                   </button>
                 </Link>
-                <Link key={p.name} href={p.link}>
+                <Link
+                  href={`geo:${p.place.coords.x},${p.place.coords.y}`}
+                  target="_blank"
+                >
                   <button className="border border-[#3b3b3b] hover:bg-[#3b3b3b] rounded-full p-2 px-3 cursor-pointer text-base flex flex-row items-center justify-center mb-2 mr-2">
                     <IconCompass className="inline mr-1" stroke={1.5} />
                     Navigovat na místo
                   </button>
                 </Link>
-                <Link key={p.name} href={p.link}>
+                <Link href={p.link}>
                   <button className="border border-[#3b3b3b] bg-[#3b3b3b83] hover:bg-[#3b3b3b] rounded-full p-2 px-3 cursor-pointer text-base flex flex-row items-center justify-center mb-2 mr-2">
                     <IconInfoCircle className="inline mr-1" stroke={1.5} />
                     Více informací
@@ -70,60 +82,58 @@ export const program = [
       "Doba rozeznají půl přesněji ve frekvence, posety uvedl u&nbsp;mixu, u&nbsp;něm tu se, nic mé rok dispozici důležitý vím. Nálada dělá sen může též sezona dob doprovází, odkazem mužskou vele vrátí z původu, uchu ty hmotné jej zdi.",
     icons: ["theater"],
     link: "/program/filharmonie",
-    place: { name: "Filhramonie Hradec Králové", coords: "" },
+    place: {
+      name: "Filhramonie Hradec Králové",
+      coords: { x: "50.2138117", y: "15.8291964" },
+      link: "https://mapy.cz/s/dedasenuge",
+    },
   },
   {
     name: "Program ve Filharmonii",
     time: "9:30",
     description:
-      "Doba rozeznají půl přesněji ve frekvence, posety uvedl u mixu, u něm tu se, nic mé rok dispozici důležitý vím. Nálada dělá sen může též sezona dob doprovází, odkazem mužskou vele vrátí z původu, uchu ty hmotné jej zdi.",
+      "Doba rozeznají půl přesněji ve frekvence, posety uvedl u&nbsp;mixu, u&nbsp;něm tu se, nic mé rok dispozici důležitý vím. Nálada dělá sen může též sezona dob doprovází, odkazem mužskou vele vrátí z původu, uchu ty hmotné jej zdi.",
     icons: ["theater"],
     link: "/program/filharmonie",
-    place: "",
+    place: {
+      name: "Filhramonie Hradec Králové",
+      coords: { x: "50.2138117", y: "15.8291964" },
+    },
   },
   {
     name: "Program ve Filharmonii",
     time: "9:30",
     description:
-      "Doba rozeznají půl přesněji ve frekvence, posety uvedl u mixu, u něm tu se, nic mé rok dispozici důležitý vím. Nálada dělá sen může též sezona dob doprovází, odkazem mužskou vele vrátí z původu, uchu ty hmotné jej zdi.",
+      "Doba rozeznají půl přesněji ve frekvence, posety uvedl u&nbsp;mixu, u&nbsp;něm tu se, nic mé rok dispozici důležitý vím. Nálada dělá sen může též sezona dob doprovází, odkazem mužskou vele vrátí z původu, uchu ty hmotné jej zdi.",
     icons: ["theater"],
     link: "/program/filharmonie",
-    place: "",
+    place: {
+      name: "Filhramonie Hradec Králové",
+      coords: { x: "50.2138117", y: "15.8291964" },
+    },
   },
   {
     name: "Program ve Filharmonii",
     time: "9:30",
     description:
-      "Doba rozeznají půl přesněji ve frekvence, posety uvedl u mixu, u něm tu se, nic mé rok dispozici důležitý vím. Nálada dělá sen může též sezona dob doprovází, odkazem mužskou vele vrátí z původu, uchu ty hmotné jej zdi.",
+      "Doba rozeznají půl přesněji ve frekvence, posety uvedl u&nbsp;mixu, u&nbsp;něm tu se, nic mé rok dispozici důležitý vím. Nálada dělá sen může též sezona dob doprovází, odkazem mužskou vele vrátí z původu, uchu ty hmotné jej zdi.",
     icons: ["theater"],
     link: "/program/filharmonie",
-    place: "",
+    place: {
+      name: "Filhramonie Hradec Králové",
+      coords: { x: "50.2138117", y: "15.8291964" },
+    },
   },
   {
     name: "Program ve Filharmonii",
     time: "9:30",
     description:
-      "Doba rozeznají půl přesněji ve frekvence, posety uvedl u mixu, u něm tu se, nic mé rok dispozici důležitý vím. Nálada dělá sen může též sezona dob doprovází, odkazem mužskou vele vrátí z původu, uchu ty hmotné jej zdi.",
+      "Doba rozeznají půl přesněji ve frekvence, posety uvedl u&nbsp;mixu, u&nbsp;něm tu se, nic mé rok dispozici důležitý vím. Nálada dělá sen může též sezona dob doprovází, odkazem mužskou vele vrátí z původu, uchu ty hmotné jej zdi.",
     icons: ["theater"],
     link: "/program/filharmonie",
-    place: "",
-  },
-  {
-    name: "Program ve Filharmonii",
-    time: "9:30",
-    description:
-      "Doba rozeznají půl přesněji ve frekvence, posety uvedl u mixu, u něm tu se, nic mé rok dispozici důležitý vím. Nálada dělá sen může též sezona dob doprovází, odkazem mužskou vele vrátí z původu, uchu ty hmotné jej zdi.",
-    icons: ["theater"],
-    link: "/program/filharmonie",
-    place: "",
-  },
-  {
-    name: "Program ve Filharmonii",
-    time: "9:30",
-    description:
-      "Doba rozeznají půl přesněji ve frekvence, posety uvedl u mixu, u něm tu se, nic mé rok dispozici důležitý vím. Nálada dělá sen může též sezona dob doprovází, odkazem mužskou vele vrátí z původu, uchu ty hmotné jej zdi.",
-    icons: ["theater"],
-    link: "/program/filharmonie",
-    place: "",
+    place: {
+      name: "Filhramonie Hradec Králové",
+      coords: { x: "50.2138117", y: "15.8291964" },
+    },
   },
 ];
