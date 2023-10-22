c89ac643995ded3d947614752248ccf3e2f77787
petrkucerak <petr@khome.cz>
2023-10-22 08:10:36 +0200

    Create program in filhamronie description

--- app/program-ve-filharmonii/page.js
+++ app/program-ve-filharmonii/page.js
@@ -4,6 +4,9 @@ import Container from "../../components/layouts/container";
 import ContainerTitle from "../../components/layouts/container-title";
 import ContainerDescription from "../../components/layouts/container-description";
 import PageMain from "../../components/layouts/page-main";
+import ProgramDetail from "../../components/layouts/program-detail";
+import { program } from "../../components/switcher/program";
+import Link from "next/link";
 export default async function ProgramVeFilharmonii() {
   return (
     <main className="ease-in-out duration-300 font-titilliumWeb">
@@ -11,7 +14,16 @@ export default async function ProgramVeFilharmonii() {
       <PageMain>
         <Container>
           <ContainerTitle>Program ve filharmonii</ContainerTitle>
-          <ContainerDescription>Program ve filharmonii...</ContainerDescription>
+          <ProgramDetail p={program[1]} className={``} />
+          <ContainerDescription>
+            Dopoledne prožijeme společně jako mladí z&nbsp;celé diecéze
+            v&nbsp;Filharmonii v&nbsp;Hradci Králové. Těšit se můžete na
+            moderovaný program, hudební doprovod od{" "}
+            <Link href="/veka" className="underline">
+              Vesmírné Kapely
+            </Link>
+            , svědectví a&nbsp;mnoho dalšího.
+          </ContainerDescription>
         </Container>
       </PageMain>
       <Footer />
---
+++ components/layouts/program-detail.js
@@ -0,0 +1,29 @@
+import ParmIcon from "./parm-icon";
+import Link from "next/link";
+export default function ProgramDetail({ className, p }) {
+  return (
+    <div
+      className={`w-full flex flex-col items-center justify-center mb-4 ${className}`}
+    >
+      <div className="flex flex-row justify-center items-center">
+        <span className="text-3xl tracking-wider">{p.time}</span>
+        <div className="ml-4 flex flex-row justify-start items-center">
+          {p.icons !== undefined
+            ? p.icons.map((i) => {
+                return <ParmIcon key={(i, p.name)} iconName={i} />;
+              })
+            : null}
+        </div>
+      </div>
+      {p.place !== undefined ? (
+        <Link
+          href={`${p.place.link}`}
+          rel="external noopener nofollow"
+          target="_blank"
+        >
+          <span className="tracking-widest underline">{p.place.name}</span>
+        </Link>
+      ) : null}
+    </div>
+  );
+}
