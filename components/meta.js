4e23efe5f960dd8769265a2fe1810b41e37b1e2e
petrkucerak <petr@khome.cz>
2023-11-08 13:06:38 +0100

    Generate splash screens

--- components/meta.js
+++ components/meta.js
@@ -41,4 +41,36 @@ export const meta = {
   },
   assets: [`${homeUrl}assets`],
   category: "non-profit",
+  openGraph: {
+    title: title,
+    description: description,
+    images: [
+      {
+        url: `${homeUrl}opengraph-image.jpg`,
+        width: 1200,
+        height: 630,
+        alt: "Diecézko 2023 v Hradci Králové",
+      },
+    ],
+    locale: "cs_CZ",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: title,
+    description: description,
+    creator: "@petrkucerak",
+    images: [`${homeUrl}twitter-image.jpg`],
+  },
+  appleWebApp: {
+    title: title,
+    statusBarStyle: "black-translucent",
+    startupImage: [
+      "/assets/startup/apple-touch-startup-image-768x1004.png",
+      {
+        url: "/assets/startup/apple-touch-startup-image-1536x2008.png",
+        media: "(device-width: 768px) and (device-height: 1024px)",
+      },
+    ],
+  },
 };
