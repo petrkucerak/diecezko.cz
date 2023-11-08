59860f85a670a2a23c8996275384d2bf1cdeeecd
Petr Kučera <petr@khome.cz>
2023-11-08 12:21:48 +0100

    Base SEO (#260)
    * Define sitemap
    
    * Define sitemap
    
    * Add OpenGraph files
    
    * Generate webp format
    
    * Add footer tag
    
    * Optimised images with calibre/image-actions
    
    ---------
    
    Co-authored-by: petrkucerak <petrkucerak@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/sitemap.js
+++ app/sitemap.js
@@ -6,5 +6,80 @@ export default function sitemap() {
       changeFrequency: "yearly",
       priority: 1,
     },
+    {
+      url: "https://diecezko.cz/evzenova-cesta",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/vikariaty",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/dcm-hradec-kralove",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/dczm-vesmir",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/kak-salas",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/studenecka-kapela",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/veka",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/jadro-diecezka",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/partneri",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/bistro-u-dvou-pratel",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/program-ve-filharmonii",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/prednasky-workshopy",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/odpoledni-program",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/mse-svata",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/tyden-modliteb-za-mladez",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
   ];
 }
