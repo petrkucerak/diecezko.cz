ff5ddcb4959a5853853ba4f2203134c9587ba001
petrkucerak <petr@khome.cz>
2023-11-08 08:26:10 +0100

    Define sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -6,5 +6,80 @@ export default function sitemap() {
       changeFrequency: "yearly",
       priority: 1,
     },
+    {
+      url: "https://diecezko.cz/evzenova-cesta",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/vikariaty",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/dcm-hradec-kralove",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/dczm-vesmir",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/kak-salas",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/studenecka-kapela",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    {
+      url: "https://diecezko.cz/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
   ];
 }
