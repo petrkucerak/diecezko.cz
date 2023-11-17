ebaa65c2aa9f4d1500d93dcf54b849ad8f5a7b48
Petr Kučera <petr@khome.cz>
2023-11-17 15:41:55 +0100

    Calendar event generation (#323)
    * Create script to build ical event
    
    * Add duration
    
    * Remove &nbsp;
    
    * Add Evzen visite
    
    * Change the type of loading the `json` file
    
    * Update the program imports

--- components/switcher/program.js
+++ components/switcher/program.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Container from "../layouts/container";
 import ParmIcon from "../layouts/parm-icon";
 import { IconArrowRight, IconCalendarPlus } from "@tabler/icons";
+import program from "./program.json";
 
 export default function Program({ id, className }) {
   return (
@@ -40,7 +41,12 @@ export default function Program({ id, className }) {
                 dangerouslySetInnerHTML={{ __html: p.description }}
               />
               <div className="flex flex-row flex-wrap items-center justify-start mt-2">
-                <Link href={`/`}>
+                <Link
+                  target="_blank"
+                  title={`Přidat ${p.name} do kalendáře`}
+                  rel="document"
+                  href={`/assets/events/${p.time.replace(":", "")}/event.ics`}
+                >
                   <button className="border border-[#3b3b3b] hover:bg-[#3b3b3b] rounded-full p-2 px-3 cursor-pointer text-base flex flex-row items-center justify-center mb-2 mr-2">
                     <IconCalendarPlus className="inline" stroke={1.5} />
                   </button>
@@ -70,68 +76,3 @@ export default function Program({ id, className }) {
     </Container>
   );
 }
-
-export const program = [
-  {
-    name: "Příjezd a registrace",
-    time: "9:00",
-    description:
-      "Při příjezdu na setkání prosím navštivte registraci, kterou najdete ve vstupu do Filharmonie. Dostanete zde náremek v barvě svého vikariátu. Už tušíte jaké to bude?",
-    icons: ["registration"],
-    place: {
-      name: "Filhramonie Hradec Králové",
-      coords: { x: "50.2138117", y: "15.8291964" },
-      link: "https://mapy.cz/s/dedasenuge",
-    },
-  },
-  {
-    name: "Program ve Filharmonii",
-    time: "9:30",
-    description:
-      "Dopolední program prožijeme společně v budově Filharmonie. Můžete se těšit na režijní scénky, svědectví, hudební doprovod v podání VeKy a mnoho dalšího.",
-    icons: ["theater", "music", "pray", "person"],
-    link: "/program-ve-filharmonii",
-    place: {
-      name: "Filhramonie Hradec Králové",
-      coords: { x: "50.2138117", y: "15.8291964" },
-      link: "https://mapy.cz/s/dedasenuge",
-    },
-  },
-  {
-    name: "Obědová pauza",
-    time: "12:00",
-    description:
-      "Při odchodu z dopoledního programu si kromě výběru workshopu nebo přednášky nezapomeňte vyzvednout i bagetu s pitíčkem. Bagety pro nás připravilo BISTRO u dvou přátel. Dobrou chuť!",
-    link: "/bistro-u-dvou-pratel",
-    icons: ["food"],
-  },
-  {
-    name: "Přednášky, workshopy",
-    time: "12:30",
-    description:
-      "Odpoledne si můžete vybrat z široké nabídky přednášek, několika workshopů nebo si jít zasportovat.",
-    icons: ["person", "workshop", "sport"],
-    link: "/prednasky-workshopy",
-  },
-  {
-    name: "Odpolední program",
-    time: "14:00",
-    description:
-      "V&nbsp;druhé části odpoledne můžete pokračovat ve sportování, navštívit kavárnu na BiGy nebo si zajít na duchovní rozhovor či svátost smíření.",
-    icons: ["pray", "cafe", "sport"],
-    link: "/odpoledni-program",
-  },
-  {
-    name: "Mše svatá",
-    time: "15:00",
-    description:
-      "Celý den zakončíme mší svatou s otcem biskupem v katedrále svatého Ducha. Mši svatou hudebně doprovodí Studenecká kapela.",
-    icons: ["music", "pray"],
-    link: "/mse-svata",
-    place: {
-      name: "Katedrála Svatého Ducha",
-      coords: { x: "50.2087878", y: "15.8310917" },
-      link: "https://mapy.cz/s/bezegujuje",
-    },
-  },
-];
---
+++ ical-generator.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const ics = require("ics");
+const program = require("./components/switcher/program.json");
+
+program.map((event) => {
+  const time = event.time.split(":");
+  ics.createEvent(
+    {
+      title: event.name,
+      url: "https://diecezko.cz/",
+      description: event.description.replaceAll("&nbsp;", " "),
+      // busyStatus: "FREE",
+      organizer: {
+        name: "Pan Holub",
+        email: "evzen@diecezko.cz",
+        dir: "https://diecezko.cz/evzenova-cesta",
+        sentBy: "evzen@diecezko.cz",
+      },
+      
+      location: event.place !== undefined ? event.place.name : undefined,
+      geo:
+        event.place !== undefined && event.place.coords !== undefined
+          ? {
+              lat: parseFloat(event.place.coords.x),
+              lon: parseFloat(event.place.coords.y),
+            }
+          : undefined,
+      start: [2023, 11, 25, parseInt(time[0]), parseInt(time[1])],
+      duration: { minutes: event.duration },
+    },
+    (error, value) => {
+      if (error) {
+        console.log(error);
+      }
+      const path = `./public/assets/events/${event.time.replace(":", "")}`;
+      if (!fs.existsSync(path)) fs.mkdirSync(path);
+      const file = fs.createWriteStream(`${path}/event.ics`);
+      file.write(value);
+      file.close();
+    }
+  );
+});
