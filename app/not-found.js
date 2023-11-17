9db4b3f1a6aa28cc0e61cace8e7e84fdf6436529
petrkucerak <petr@khome.cz>
2023-11-17 16:01:30 +0100

    Create the 404 page

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+export default function ErrorPage() {
+  return (
+    <div className="flex flex-col items-center min-h-[100vh] justify-center">
+      <img src="/assets/images/404.png" width={100} height={100} />
+      <h1 className="font-mono text-4xl font-bold mt-2">404 code</h1>
+      <p className="text-2xl mb-1">Upppsssss...</p>
+      <p>Vypadá to, že takhle stránka někam ufrnkla :(</p>
+    </div>
+  );
+}
