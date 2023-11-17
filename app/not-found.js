2d5836ab4957ff0b99d5ebf91bb217b8c250b592
petrkucerak <petr@khome.cz>
2023-11-17 16:04:09 +0100

    Unify the font

--- app/not-found.js
+++ app/not-found.js
@@ -1,6 +1,6 @@
 export default function ErrorPage() {
   return (
-    <div className="flex flex-col items-center min-h-[100vh] justify-center">
+    <div className="flex flex-col items-center min-h-[100vh] justify-center font-titilliumWeb">
       <img src="/assets/images/404.png" width={100} height={100} />
       <h1 className="font-mono text-4xl font-bold mt-2">404 code</h1>
       <p className="text-2xl mb-1">Upppsssss...</p>
