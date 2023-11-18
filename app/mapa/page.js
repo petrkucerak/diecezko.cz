2d7242e498fec5a24bbb73c22682122c6776e420
petrkucerak <petr@khome.cz>
2023-11-18 09:37:22 +0100

    Can load map properelly

--- app/mapa/page.js
+++ app/mapa/page.js
@@ -2,7 +2,13 @@
 import Header from "../../components/header";
 import Footer from "../../components/footer";
 import PageMain from "../../components/layouts/page-main";
-import Map from "../../components/map";
+// import Map from "../../components/map";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("../../components/map"), {
+  loading: () => <p>Načítání...</p>,
+  ssr: false,
+});
 export default function Mapa() {
   return (
     <main className="ease-in-out duration-300 font-titilliumWeb">
--- components/map.js
+++ components/map.js
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapConsumer, MapContainer, TileLayer } from "react-leaflet";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet/dist/leaflet.css";
@@ -6,17 +6,18 @@ import MapLocation from "./map-location";
 import MapOffline from "./map-offline";
 
 export default function Map() {
-  return (
-    <MapContainer
-      center={[50.213, 15.832]}
-      zoom={16}
-      minZoom={15}
-      scrollWheelZoom={true}
-      className="w-screen h-[70vh] diseable-map-selection z-0"
-    >
-      <TileLayer url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" />
-      {/* <MapLocation />
+  if (typeof window !== undefined)
+    return (
+      <MapContainer
+        center={[50.213, 15.832]}
+        zoom={16}
+        minZoom={15}
+        scrollWheelZoom={true}
+        className="w-screen h-[70vh] diseable-map-selection z-0"
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" />
+        {/* <MapLocation />
       <MapOffline /> */}
-    </MapContainer>
-  );
+      </MapContainer>
+    );
 }
