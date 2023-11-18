0101406c27b247c63e506ea6e0b3314327451263
petrkucerak <petr@khome.cz>
2023-11-18 14:13:23 +0100

    Add page to sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -81,5 +81,11 @@ export default function sitemap() {
       lastModified: new Date(),
       changeFrequency: "yearly",
     },
+
+    {
+      url: "https://diecezko.cz/mapa",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
   ];
 }
