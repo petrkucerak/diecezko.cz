5c8fdf017a8a61aa6adc59ea3c2ec889688b9f82
petrkucerak <petr@khome.cz>
2023-11-21 15:42:32 +0100

    Define custom cache policies

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const runtimeCaching = require("next-pwa/cache");
+const runtimeCaching = require("./custom-cache");
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
