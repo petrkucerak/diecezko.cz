eb2c2f077be11623e8567246419c09cf77bf9106
petrkucerak <petr@khome.cz>
2023-11-21 15:49:20 +0100

    Define custom policies

--- custom-cache.js
+++ custom-cache.js
@@ -3,35 +3,13 @@
 // Workbox RuntimeCaching config: https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-build#.RuntimeCachingEntry
 module.exports = [
   {
-    urlPattern: /^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,
-    handler: "CacheFirst",
-    options: {
-      cacheName: "google-fonts-webfonts",
-      expiration: {
-        maxEntries: 4,
-        maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
-      },
-    },
-  },
-  {
-    urlPattern: /^https:\/\/fonts\.(?:googleapis)\.com\/.*/i,
-    handler: "StaleWhileRevalidate",
-    options: {
-      cacheName: "google-fonts-stylesheets",
-      expiration: {
-        maxEntries: 4,
-        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
-      },
-    },
-  },
-  {
-    urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+    urlPattern: /\.(?:woff|woff2)$/i,
     handler: "StaleWhileRevalidate",
     options: {
       cacheName: "static-font-assets",
       expiration: {
-        maxEntries: 4,
-        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+        maxEntries: 26,
+        maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
       },
     },
   },
@@ -42,7 +20,7 @@ module.exports = [
       cacheName: "static-image-assets",
       expiration: {
         maxEntries: 64,
-        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
       },
     },
   },
@@ -53,7 +31,7 @@ module.exports = [
       cacheName: "next-image",
       expiration: {
         maxEntries: 64,
-        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
       },
     },
   },
--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
   // scope: '/app',
   // sw: 'service-worker.js',
-  runtimeCaching,
+  runtimeCaching: runtimeCaching,
   cacheOnFrontEndNav: true,
   //...
 });
