323efbb95eafb70f02b18a7a9c77084f5ef6401e
petrkucerak <petr@khome.cz>
2023-11-22 08:17:46 +0100

    Config caching policies

--- custom-cache.js
+++ custom-cache.js
@@ -4,33 +4,33 @@
 module.exports = [
   {
     urlPattern: /\.(?:woff|woff2)$/i,
-    handler: "StaleWhileRevalidate",
+    handler: "CacheFirst",
     options: {
       cacheName: "static-font-assets",
       expiration: {
-        maxEntries: 26,
+        maxEntries: 200,
         maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
       },
     },
   },
   {
     urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-    handler: "StaleWhileRevalidate",
+    handler: "CacheFirst",
     options: {
       cacheName: "static-image-assets",
       expiration: {
-        maxEntries: 64,
+        maxEntries: 200,
         maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
       },
     },
   },
   {
     urlPattern: /\/_next\/image\?url=.+$/i,
-    handler: "StaleWhileRevalidate",
+    handler: "CacheFirst",
     options: {
       cacheName: "next-image",
       expiration: {
-        maxEntries: 64,
+        maxEntries: 200,
         maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
       },
     },
@@ -42,8 +42,8 @@ module.exports = [
       rangeRequests: true,
       cacheName: "static-audio-assets",
       expiration: {
-        maxEntries: 32,
-        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        maxEntries: 200,
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 24 hours
       },
     },
   },
@@ -54,52 +54,52 @@ module.exports = [
       rangeRequests: true,
       cacheName: "static-video-assets",
       expiration: {
-        maxEntries: 32,
-        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        maxEntries: 200,
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 24 hours
       },
     },
   },
   {
     urlPattern: /\.(?:js)$/i,
-    handler: "StaleWhileRevalidate",
+    handler: "CacheFirst",
     options: {
       cacheName: "static-js-assets",
       expiration: {
-        maxEntries: 32,
-        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        maxEntries: 200,
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 24 hours
       },
     },
   },
   {
     urlPattern: /\.(?:css|less)$/i,
-    handler: "StaleWhileRevalidate",
+    handler: "CacheFirst",
     options: {
       cacheName: "static-style-assets",
       expiration: {
-        maxEntries: 32,
-        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        maxEntries: 200,
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 24 hours
       },
     },
   },
   {
     urlPattern: /\/_next\/data\/.+\/.+\.json$/i,
-    handler: "StaleWhileRevalidate",
+    handler: "CacheFirst",
     options: {
       cacheName: "next-data",
       expiration: {
-        maxEntries: 32,
-        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        maxEntries: 200,
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 24 hours
       },
     },
   },
   {
     urlPattern: /\.(?:json|xml|csv)$/i,
-    handler: "NetworkFirst",
+    handler: "CacheFirst",
     options: {
       cacheName: "static-data-assets",
       expiration: {
-        maxEntries: 32,
-        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        maxEntries: 200,
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 24 hours
       },
     },
   },
