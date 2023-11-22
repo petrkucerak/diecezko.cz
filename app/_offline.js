259bfd3cec8c34a8580fb9000e47eedac8db5875
petrkucerak <petr@khome.cz>
2023-11-22 12:58:08 +0100

    Define offline page

---
+++ app/_offline.js
@@ -0,0 +1,16 @@
+import Cover from "../components/cover";
+import Header from "../components/header";
+import Switcher from "../components/switcher";
+import Footer from "../components/footer";
+
+export default function Home() {
+  return (
+    <main className="ease-in-out duration-300 font-titilliumWeb">
+      <Header />
+      <Cover>
+        <Switcher />
+      </Cover>
+      <Footer />
+    </main>
+  );
+}
