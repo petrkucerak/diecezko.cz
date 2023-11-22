5e69aa4e6c31b3ee80793c3bf98bd6302362d8c7
petrkucerak <petr@khome.cz>
2023-11-22 12:46:51 +0100

    Update manifest

--- app/manifest.js
+++ app/manifest.js
@@ -7,10 +7,18 @@ export default function manifest() {
     description: meta.description,
     start_url: "/",
     display: "standalone",
+    orientation: "portrait-primary",
     background_color: "#000",
     scope: "/",
     lang: "cs",
     theme_color: meta.themeColor,
+    categories: [
+      "education",
+      "lifestyle",
+      "magazines",
+      "personalization",
+      "social",
+    ],
     icons: [
       { src: "/icon-192.png", type: "image/png", sizes: "192x192" },
       { src: "/icon-512.png", type: "image/png", sizes: "512x512" },
