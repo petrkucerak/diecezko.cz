3cf0bc8164b330a74bedec8534e2f4c047bea069
Petr Kučera <petr@khome.cz>
2023-11-24 00:31:56 +0100

    Improve offline callback page (#431)
    * Try new precache strategy
    
    * Add offline notice

--- app/~offline/page.js
+++ app/~offline/page.js
@@ -7,6 +7,10 @@ export default function Home() {
   return (
     <main className="ease-in-out duration-300 font-titilliumWeb">
       <Header />
+      <div className="w-full text-center bg-yellow-300 text-black py-1 font-titilliumWeb">
+        <strong>(Offline)</strong> Pro plnou funkčnost se připoj
+        k&nbsp;internetu...
+      </div>
       <Cover>
         <Switcher />
       </Cover>
