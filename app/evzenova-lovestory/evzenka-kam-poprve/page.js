ef66e31a4158b3aef767ea23175bca5eef50caa5
petrkucerak <petr@khome.cz>
2023-11-24 14:28:29 +0100

    Add Evzenie introduce story

--- app/evzenova-lovestory/evzenka-kam-poprve/page.js
+++ app/evzenova-lovestory/evzenka-kam-poprve/page.js
@@ -17,11 +17,28 @@ export default function Evzen() {
             #KamPoprve
           </ContainerDescription>
           <ContainerH2>Příběh</ContainerH2>
-          TODO
+          <ContainerParagraph>
+            Zdravím vás týme. Rozhodli jste se, že se mnou prožijete
+            dobrodružství. Nebude to jednoduché, ale věřím, že když budeme
+            spolupracovat, máme šanci vše zdárně dokončit. Nezapomeňte, že celou
+            cestu musím úspěšně dokončit do 14:55.
+          </ContainerParagraph>
+          <ContainerParagraph>
+            Minulý týden jsem si zobala na náměstí. Tradičně v&nbsp;obležení
+            mých bratrů a&nbsp;sester, kteří jsou pouze tupec vedle tupce. Myslí
+            neustále jen na jídlo, zob a&nbsp;drob. A&nbsp;to je taková nuda!
+            Tentokráte se ale stalo něco zvláštního. Přiletěl jeden holub,
+            nevím, jak se jmenuje. Na první pohled stejný, jako všichni ostatní.
+            Když jsem mu ale pohlédla do tváře, zatrnulo ve mně. Nenechala jsem
+            na sobě nic znát. Jeho oči na mě jeho zapůsobili jako magnet. Ta
+            zvídavost a&nbsp;touha vědění v&nbsp;nich byla neúprosná, že jsem se
+            radě zvedla a&nbsp;odlétla pryč. Po cestě mě ale přepadli silné
+            severní větry, které mě zanesli do oblastí, v&nbsp;kterých se zas
+            tak nevyznám. Potřebuji se vydat na nějaké známé místo…
+          </ContainerParagraph>
           <ContainerH2>Instrukce</ContainerH2>
           <ContainerParagraph>
-            Vydejte se k&nbsp;UHK na přesnou souřadnici TODO. Tam se dozvíte
-            další instrukce.
+            Vydejte se na známé místo Evženie, které najdete: TODO.
           </ContainerParagraph>
         </Container>
       </PageMain>
