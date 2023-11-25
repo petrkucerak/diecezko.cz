2961141e9ed6f0898d1c98b788a58127a99848e9
petrkucerak <petr@khome.cz>
2023-11-25 12:26:56 +0100

    Update content

--- app/evzenova-lovestory/evzenka-z-bona-na-jedno/page.js
+++ app/evzenova-lovestory/evzenka-z-bona-na-jedno/page.js
@@ -1,3 +1,4 @@
+"use client";
 import Header from "../../../components/header";
 import Footer from "../../../components/footer";
 import Container from "../../../components/layouts/container";
@@ -6,7 +7,14 @@ import PageMain from "../../../components/layouts/page-main";
 import ContainerDescription from "../../../components/layouts/container-description";
 import ContainerH2 from "../../../components/layouts/container-h2";
 import ContainerParagraph from "../../../components/layouts/container-paragraph";
+import Button from "../../../components/layouts/button";
+import { useState } from "react";
 export default function Evzen() {
+  const [value, setValue] = useState("");
+  const [status, setStatus] = useState("hidden");
+  const validate = () => {
+    value === "inverze" ? setStatus("") : setStatus("hidden");
+  };
   return (
     <main className="ease-in-out duration-300 font-titilliumWeb">
       <Header />
@@ -23,14 +31,30 @@ export default function Evzen() {
             přicházet temné myšlenky. Co když se na Vás ani nepodívá, co když si
             o&nbsp;vás pomyslí něco špatného, co když ji s&nbsp;vámi nebude
             dobře? Co když nejste dost krásná? Víte, že to je jen o&nbsp;hlavě
-            a&nbsp;že to musíte překonat.
+            a&nbsp;že to musíte překonat. Abyste si zvedli sebevědomí, chcete si
+            ověřit, že máte dobrou pamět. Vzpomínáte, že dopoledne na programu
+            Marty mluvil o&nbsp;jevu, kdy je na horách hezky
+            a&nbsp;v&nbsp;nížině škaredo. Jak se jmenuje? Pokud se strefíte,
+            pozvednte si sebevědomí a&nbsp;zjistíte kam a&nbsp;jak za Evženií.
           </ContainerParagraph>
-          <ContainerH2>Instrukce</ContainerH2>
-          <ContainerParagraph>
-            Pospěšte, natáhněte krok a&nbsp;co nejrychleji bez ohlížení
-            a&nbsp;pochybností se dostaňte na místo (50.2080458N, 15.8292844E),
-            kde na vás snad již bude čekat Evžen.
-          </ContainerParagraph>
+          <input
+            type="text"
+            id="input"
+            placeholder="Vložte odpověď malým písmem"
+            className="p-2 px-3 rounded-xl w-full"
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <div onClick={validate} className="mt-2">
+            <Button>Validovat</Button>
+          </div>
+          <div className={`${status}`}>
+            <ContainerH2>Instrukce</ContainerH2>
+            <ContainerParagraph>
+              Pospěšte, natáhněte krok a&nbsp;co nejrychleji bez ohlížení
+              a&nbsp;pochybností se dostaňte na místo (50.2080131N,
+              15.8292764E), kde na vás snad již bude čekat Evženie.
+            </ContainerParagraph>
+          </div>
         </Container>
       </PageMain>
       <Footer />
