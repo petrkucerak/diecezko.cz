651be51ac9c62464011c6b8a75d8b7c292c0548a
petrkucerak <petr@khome.cz>
2023-11-27 17:05:42 +0100

    Update images

--- app/galerie/page.js
+++ app/galerie/page.js
@@ -7,8 +7,20 @@ import ContainerDescription from "../../components/layouts/container-description
 import PageMain from "../../components/layouts/page-main";
 import Link from "next/link";
 import Gallery from "react-photo-gallery-next";
+import { useCallback, useState } from "react";
 import { photos } from "../../components/photos";
 export default function Galerie() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
   return (
     <main className="ease-in-out duration-300 font-titilliumWeb">
       <Header />
@@ -36,7 +48,7 @@ export default function Galerie() {
             </Link>
             .
           </ContainerDescription>
-          <Gallery photos={photos} margin={2} />
+          <Gallery photos={photos} margin={2} onClick={openLightbox} />
         </Container>
       </PageMain>
       <Footer />
