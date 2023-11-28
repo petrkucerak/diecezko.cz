2523db030f7728e14e171cbf1054471215946fee
petrkucerak <petr@khome.cz>
2023-11-28 12:39:40 +0100

    Add new images

--- app/galerie/page.js
+++ app/galerie/page.js
@@ -7,11 +7,18 @@ import ContainerDescription from "../../components/layouts/container-description
 import PageMain from "../../components/layouts/page-main";
 import Link from "next/link";
 import { photos } from "../../components/photos";
-import Lightbox from "yet-another-react-lightbox";
-import "yet-another-react-lightbox/styles.css";
 import { useState } from "react";
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/counter.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import Counter from "yet-another-react-lightbox/plugins/counter";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Lightbox from "yet-another-react-lightbox";
+import PhotoAlbum from "react-photo-album";
+
 export default function Galerie() {
-  const [open, setOpen] = useState(true);
+  const [index, setIndex] = useState(-1);
   return (
     <main className="ease-in-out duration-300 font-titilliumWeb">
       <Header />
@@ -38,10 +45,19 @@ export default function Galerie() {
               Honza Urban
             </Link>
             .
+            <PhotoAlbum
+              layout="masonry"
+              photos={photos}
+              onClick={({ index: current }) => setIndex(current)}
+            />
             <Lightbox
-              open={open}
-              close={() => setOpen(false)}
+              open={index >= 0}
+              index={index}
+              close={() => setIndex(-1)}
               slides={photos}
+              plugins={[Counter, Fullscreen, Thumbnails]}
+              counter={{ container: { style: { top: "unset", bottom: 0 } } }}
+              thumbnails={{ border: 0, gap: 4 }}
             />
           </ContainerDescription>
         </Container>
