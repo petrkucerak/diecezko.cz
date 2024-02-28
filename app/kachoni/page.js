aa4726d3c9cf88b78160074347e10286e41f20e3
petrkucerak <petr@khome.cz>
2024-02-28 10:29:27 +0100

    Remove unused code

--- app/kachoni/page.js
+++ app/kachoni/page.js
@@ -6,17 +6,6 @@ import ContainerDescription from "../../components/layouts/container-description
 import ContainerParagraph from "../../components/layouts/container-paragraph";
 import PageMain from "../../components/layouts/page-main";
 import ContainerImage from "../../components/layouts/container-image";
-import ContainerH2 from "../../components/layouts/container-h2";
-import ContainerUl from "../../components/layouts/container-ul";
-import ContainerLi from "../../components/layouts/container-li";
-import Link from "next/link";
-import {
-  IconBrandFacebook,
-  IconBrandInstagram,
-  IconBrandSpotify,
-  IconBrandYoutube,
-  IconWorld,
-} from "@tabler/icons";
 export default async function Kachnoni() {
   return (
     <main className="ease-in-out duration-300 font-titilliumWeb">
