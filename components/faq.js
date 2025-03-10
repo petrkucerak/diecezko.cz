165f558fc71081a9bb947b1159c1a82c2309e1ef
petrkucerak <petr@khome.cz>
2025-03-10 18:09:04 +0100

    Remove irrelevant text

--- components/faq.js
+++ components/faq.js
@@ -109,16 +109,6 @@ export default function FAQ() {
         , který poradí, kdy a&nbsp;v&nbsp;kolik je sraz a&nbsp;jestli je nutné
         se někam hlásit.
       </FaqBox> */}
-      <FaqBox
-        question={`Proč je Diecézko po tak brzké době?`}
-        icon={`calendar`}
-      >
-        Pamětníci nám dají za pravdu, že tradiční termín jest na jaře, kdy se
-        setkání v&nbsp;minulosti konalo. Nejde tedy o&nbsp;změnu, že by se
-        Diecézko nově konalo 2x během školního roku, nýbrž o&nbsp;změnu termínu
-        (přesun z&nbsp;podzimu zpět na jaro). Další se bude konat až na jaře
-        2025 a&nbsp;přesný termín bude zveřejněn během tohoto setkání.
-      </FaqBox>
       <FaqBox question={`Jak se v Hradci dopravovat?`} icon={`bus`}>
         <ContainerH3>Stručně</ContainerH3>
         Hradec Králové je malej, <strong>choďte pěšky</strong>!
