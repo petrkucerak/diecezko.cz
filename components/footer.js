fac2aa982f8ed51b9634054644006f0fa0465518
Petr Kučera <petr@khome.cz>
2025-03-10 18:45:26 +0100

    Add link to historic 2024 version (#572)

--- components/footer.js
+++ components/footer.js
@@ -22,13 +22,21 @@ export default function Footer() {
             DCM Hradec Králové
           </Link>
         </p>
-        <div className="my-4 underline text-xs uppercase">
+        <div className="my-4 text-xs uppercase">
           <Link
             href="https://2023.diecezko.cz/"
             target="_blank"
-            className="cursor-pointer"
+            className="cursor-pointer underline"
           >
             Diecézko podzim 2023
+          </Link>
+          {" • "}
+          <Link
+            href="https://2024.diecezko.cz/"
+            target="_blank"
+            className="cursor-pointer underline"
+          >
+            Diecézko jaro 2024
           </Link>
         </div>
         <div className="flex flex-row items-center justify-around min-w-[150px]">
