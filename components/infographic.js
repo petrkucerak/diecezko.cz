3251269684724aee62d489a581d016bf69ae03fb
petrkucerak <petr@khome.cz>
2025-03-10 19:40:11 +0100

    Fix typo

---
+++ components/infographic.js
@@ -0,0 +1,10 @@
+import ContainerH2 from "./layouts/container-h2";
+
+export default function Infographic() {
+  return (
+    <div>
+      <ContainerH2>Co Tě na setkání čeká?</ContainerH2>
+      
+    </div>
+  );
+}
