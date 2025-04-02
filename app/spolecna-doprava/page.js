cfed4b1a2e9f76b411b610e3b826529d3b8f11f9
Jindřich Řezníček <147995880+jlhotar@users.noreply.github.com>
2025-04-02 18:21:15 +0200

    Update information about transportation from vicariate (#639)
    * Add trasnport from vicariate Jicin
    
    * Add trasnport from vicariate Litomysl
    
    * Update Jicin
    
    * Add transport from vicariate Nachod
    
    * Generate webp format
    
    * Fix typo images

--- app/spolecna-doprava/page.js
+++ app/spolecna-doprava/page.js
@@ -43,6 +43,52 @@ export default async function GroupTransport() {
                 className="w-full my-4"
               />
             </FaqBox>
+            <FaqBox question={`Jičín`} icon={``}>
+              Společná doprava s&nbsp;vikariátem Jičín je vlakem Os&nbsp;5503.<br />
+              7:20 Libuň<br />
+              7:35 Jičín<br />
+              8:01 Ostroměř<br />
+              8:12 Hořice v&nbsp;Podkrkonoší
+              <ContainerImage
+                jpgPath="/assets/images/preddiecezka/preddiecezko-jicin.jpg"
+                webpPath="/assets/images/preddiecezka/preddiecezko-jicin.webp"
+                altText="Plakátek s informacemi o PředDiecézku."
+                className="w-full my-4"
+              />
+            </FaqBox>
+            <FaqBox question={`Litomyšl`} icon={``}>
+              Společná doprava s&nbsp;vikariátem Litomyšl je objednaným 
+              autobusem. Přihlášení a&nbsp;další informace o&nbsp;společné 
+              dopravě jsou k&nbsp;dispozici na{" "}
+              <Link
+                className="underline"
+                href={`https://forms.gle/sAPYnuNuS1uZUXvU6`}
+                target="_blank"
+                rel="external noopener nofollow"
+              >
+                odkaze zde
+              </Link>
+              .
+              <br />
+              <br />              
+              7:25 Polička, aut. st., výstupní zastávka (před vlak. nádr.)<br />
+              7:35 Sebranice, ob. ú.<br />
+              7:40 Horní Újezd, Černý mlýn<br />
+              7:45 Dolní Újezd, pošta<br />
+              7:55 Litomyšl, aut. nádr.<br />
+              8:05 Cerekvice n. L., rozc.0.1.<br />
+            </FaqBox>
+            <FaqBox question={`Náchod`} icon={``}>
+              Společná doprava s&nbsp;vikariátem Náchod je vlaky:<br />
+              Sp&nbsp;1855/1385 s&nbsp;odjezdem z&nbsp;Náchoda v&nbsp;8:03;<br />
+              Sp&nbsp;1385 s&nbsp;odjezdem z&nbsp;Červeného Kostelce v&nbsp;8:10;<br />
+              autobusem z&nbsp;Nového Města nad Metují, Na&nbsp;Rychtě 
+              s&nbsp;odjezdem v&nbsp;7:35.<br />
+              <br />
+              Pro více informací kontaktujte:<br /> 
+              o.&nbsp;Honzu Pecháčka - 773&nbsp;244&nbsp;919,<br /> 
+              Michala Matysku - 776&nbsp;132&nbsp;288.
+            </FaqBox>
             <FaqBox question={`Pardubice`} icon={``}>
               <ContainerImage
                 jpgPath="/assets/images/doprava/doprava-pce.jpg"
